0a771e0ea3c4ce9fd46c40ce9d12ddf33f314d45
Emilio Tirado <emilio.tirado57@gmail.com>
2017-02-09 15:14:55 -0400

    fresh start

--- app/index.js
+++
@@ -1,9 +0,0 @@
-import _ from 'lodash';
-
-function component () {
-    var element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
-    return element;
-}
-
-document.body.appendChild(component());
