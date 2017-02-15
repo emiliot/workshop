c1b6d6ac65d5f987db70fd6f37d362fab6360d21
Emilio Tirado <emilio.tirado57@gmail.com>
2017-02-15 17:33:01 -0400

    building webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: './main.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+    resolve: {
+        // you can now require('file') instead of require('file.js')
+        extensions: ['', '.js', '.json', 'jsx']
+    }
+};
