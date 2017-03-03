b2c672b78ef69890fbb5e126240bb9769bab556c
Emilio Tirado <emilio.tirado57@gmail.com>
2017-03-03 16:02:02 -0400

    cleaned master

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import styles from './styles.css';
+
+class App extends Component {
+    render () {
+        return (
+            <div className="app-blue">
+                <h1>Hello, emilio</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
