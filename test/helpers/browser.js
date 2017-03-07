84534b83860fb8ad056da64dd68f3201e5ba7e02
Emilio Tirado <emilio.tirado57@gmail.com>
2017-03-07 17:06:20 -0400

    work in progress

--- test/helpers/browser.js
+++ test/helpers/browser.js
@@ -1,7 +1,9 @@
 require('babel-register')();
+
 var jsdom = require('jsdom').jsdom;
 
 var exposedProperties = ['window', 'navigator', 'document'];
+
 global.document = jsdom('');
 global.window = document.defaultView;
 Object.keys(document.defaultView).forEach((property) => {
@@ -12,8 +14,7 @@ Object.keys(document.defaultView).forEach((property) => {
 });
 
 global.navigator = {
-    userAgent: 'node.js',
-    plugins: []
+    userAgent: 'node.js'
 };
 
 documentRef = document;
