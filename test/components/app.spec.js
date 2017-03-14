b5a264c95c6c255556965e5f308512fa6c2980c5
Emilio Tirado <emilio.tirado57@gmail.com>
2017-03-14 16:15:04 -0400

    adding linting to styles and editorconfig

--- test/components/app.spec.js
+++ test/components/app.spec.js
@@ -4,8 +4,8 @@ import {expect} from 'chai';
 import App from '../../src/components/app';
 
 describe('<App> base test', function () {
-    it('should render base component', function () {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find('h1')).to.have.length(1);
-    })
+  it('should render base component', function () {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h1')).to.have.length(1);
+  })
 })
